a04549bfa71663690c0e5cdf9b7699ff976848c5
nardo88 <nardo1988@mail.ru>
2022-08-27 22:39:14 +0300

    feat: свертал вложенные картинки в сообщении

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -4,6 +4,11 @@ import dayjs from 'dayjs'
 import ReadIcon from '../icons/Read'
 import NotReadIcon from '../icons/NotRead'
 
+interface FileType {
+  filename: string
+  url: string
+}
+
 interface MessageProps {
   user: {
     fullName: string
@@ -13,6 +18,7 @@ interface MessageProps {
   content: string
   isMe?: boolean
   isRead?: boolean
+  attachments?: FileType[]
 }
 
 const Message: FC<MessageProps> = ({
@@ -22,6 +28,7 @@ const Message: FC<MessageProps> = ({
   isMe,
   user,
   isRead,
+  attachments,
 }) => {
   return (
     <div className={`message ${isMe ? 'message--isme' : ''}`}>
@@ -33,6 +40,15 @@ const Message: FC<MessageProps> = ({
           className={isMe ? 'message__buble--isme' : 'message__buble--notme'}>
           <p className="message__text">{content}</p>
         </div>
+        {attachments && (
+          <div className="message__images">
+            {attachments.map((item: FileType) => (
+              <div className="message__image" key={item.url}>
+                <img src={item.url} alt={item.filename} />
+              </div>
+            ))}
+          </div>
+        )}
         <div className="df aic mt4">
           {isMe && <span> {isRead ? <ReadIcon /> : <NotReadIcon />}</span>}
           <span className={`message__date ${isMe && 'ml8'}`}>
--- client/src/pages/Test/index.tsx
+++ client/src/pages/Test/index.tsx
@@ -1,6 +1,12 @@
 import Message from '../../components/Message'
 import dayjs from 'dayjs'
 
+const url1 = 'https://uproger.com/wp-content/uploads/2021/12/cover-a1d5b40.png'
+const url2 =
+  'https://fuzeservers.ru/wp-content/uploads/a/6/d/a6d05477e67baf048d57ff6d15696f56.jpeg'
+const url3 =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/800px-Typescript_logo_2020.svg.png'
+
 const Test = () => {
   const message = {
     user: {
@@ -19,7 +25,16 @@ const Test = () => {
 
       <Message {...message} isMe={true} isRead={true} />
       <Message {...message} />
-      <Message {...message} isMe={true} isRead={false} />
+      <Message
+        {...message}
+        isMe={true}
+        isRead={false}
+        attachments={[
+          { filename: 'React1', url: url1 },
+          { filename: 'Node', url: url2 },
+          { filename: 'TypeScript', url: url3 },
+        ]}
+      />
     </div>
   )
 }
