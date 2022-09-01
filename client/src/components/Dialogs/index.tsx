c1694a399508ef0b2181259a98ea606f31fced79
nardo88 <nardo1988@mail.ru>
2022-09-01 21:58:16 +0300

    2

--- client/src/components/Dialogs/index.tsx
+++ client/src/components/Dialogs/index.tsx
@@ -10,15 +10,7 @@ const Dialogs: FC<DialogItemProps> = ({ items, ownerId }) => {
   return (
     <div>
       {items.map((item: any) => (
-        <DialogItem
-          key={item._id}
-          isMe={item.user._id === ownerId}
-          user={item.user}
-          text={item.text}
-          isReaded={item.isReaded}
-          createdAt={item.createdAt}
-          count={item.count}
-        />
+        <DialogItem key={item._id} isMe={item.user._id === ownerId} {...item} />
       ))}
     </div>
   )
